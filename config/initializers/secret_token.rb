d5d0137a550da964063d5c04a26e77ff7907e6a4
Taera DeFreese <taeradefreese@Taeras-Airbook.local>
2013-03-28 17:51:41 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '5ca203c02648056047bb99b9106f0a825e274c8065281a439ccbea381a970148aa6086842c34999a429002c1cd62ac67b0c18791a7b4e8bf55fae6ffe07e5896'
